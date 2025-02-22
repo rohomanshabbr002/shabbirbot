6b9589a5128f65156867dfb626a44d4140071c64
rohomanshabbr002 <odixamomi1658@gmail.com>
2025-02-22 22:35:24 +0600

    Add files via upload

---
+++ TINA/commands/gist-mirai.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const baseApiUrl = async () => {
+  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
+  return base.data.api;
+};
+
+module.exports.config = {
+  name: "gist",
+  version: "6.9.0",
+  hasPermission: 2,
+  credits: "dipto",
+  usePrefix: true,
+  prefix: true,
+  description: "Convert code into link",
+  commandCategory: "convert",
+  category: " convert",
+  usages: "[filename]/[reply and file name]",
+  cooldowns: 1
+};
+
+module.exports.run = async function ({ api, event, args }) {
+  const admin = ["your uid here"];
+  const fileName = args[0];
+
+  if (!admin.includes(event.senderID)) {
+    api.sendMessage("⚠ | You do not have permission to use this command.", event.threadID, event.messageID);
+    return;
+  }
+
+  const path = `modules/commands/${fileName}.js`;
+  try {
+    let code = '';
+
+    if (event.type === "message_reply") {
+      code = event.messageReply.body;
+    } else {
+      code = await fs.promises.readFile(path, 'utf-8');
+    }
+
+    const en = encodeURIComponent(code);
+
+    const response = await axios.post(`${await baseApiUrl()}/gist`, {
+      code: en,
+      nam: `${fileName}.js`
+    });
+
+    const diptoUrl = response.data.data;
+    api.sendMessage(diptoUrl, event.threadID, event.messageID);
+
+  } catch (error) {
+    console.error("An error occurred:", error);
+    api.sendMessage("Error occurred while processing the command.", event.threadID, event.messageID);
+  }
+};
