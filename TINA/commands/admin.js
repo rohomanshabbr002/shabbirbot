beb9307bb35d44a3c6344049070aa4d260a8a797
rohomanshabbr002 <odixamomi1658@gmail.com>
2025-02-22 23:53:45 +0600

    Update admin.js

--- TINA/commands/admin.js
+++ TINA/commands/admin.js
@@ -25,18 +25,18 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
-var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
+var callback = () => api.sendMessage({body:`𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡✨
 ======================
-𝗔𝗗𝗠𝗜𝗡 :𝗡𝗔𝗭𝗥𝗨𝗟 
-𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
+𝗔𝗗𝗠𝗜𝗡 :𝗦𝗛𝗔𝗕𝗕𝗜𝗥 𝗛𝗨𝗦𝗔𝗜𝗡 𝗺𝘂𝗻𝗻𝘀
+𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𒆜𝓘 𝓵𝓸𝓿𝓮💓𝓲𝓼𝓵𝓪𝓶ツ
 ======𝗖𝗢𝗡𝗧𝗔𝗖𝗧======
 ======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com
 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
-𝗪𝗢𝗡𝗘𝗥: 𝗡𝗔𝗭𝗥𝗨𝗟 
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+https://m.me
+𝗪𝗢𝗡𝗘𝗥: 𝗦𝗛𝗔𝗕𝗕𝗜𝗥
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000350435589/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100085961271287/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
